2e36352e929d43e2e70510b95af64d6ae01ce67f
Christian Kaisermann <christian@kaisermann.me>
2019-04-08 21:35:04 -0300

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -3,18 +3,14 @@ import {
   signedDistanceToCircle,
   truncateBetween,
   degToRad,
+  getSegmentVector,
 } from './utils.js';
 
 const KEYS_PRESSED = {};
-const isMovementKey = key =>
-  key === 'w' ||
-  key === 's' ||
-  key === 'd' ||
-  key === 'a' ||
-  key === 'arrowleft' ||
-  key === 'arrowright' ||
-  key === 'arrowup' ||
-  key === 'arrowdown';
+const MOUSE = {
+  position: { x: null, y: false },
+  isDown: false,
+};
 
 let animationRequest;
 let canvasDiagonalLength;
@@ -23,12 +19,12 @@ const canvas = document.querySelector('.canvas');
 const ctx = canvas.getContext('2d');
 
 const obstacles = [
-  { center: [500, 200], radius: 50, color: 'red' },
-  { center: [800, 100], radius: 100, color: 'yellow' },
-  { center: [100, 300], radius: 80, color: 'blue' },
-  { center: [150, 50], radius: 40, color: 'green' },
-  { center: [500, 800], radius: 120, color: 'gold' },
-  { center: [190, 600], radius: 70, color: 'orange' },
+  { center: [500, 200], radius: 50, color: '#F08080' },
+  { center: [800, 100], radius: 100, color: '#FFF176' },
+  { center: [100, 300], radius: 80, color: '#D8BFD8' },
+  { center: [150, 50], radius: 40, color: '#A5D6A7' },
+  { center: [500, 800], radius: 120, color: '#880E4F' },
+  { center: [190, 600], radius: 70, color: '#0097A7' },
 ];
 
 const hero = {
@@ -62,20 +58,21 @@ const hero = {
     this.updatePosition([nx, ny]);
   },
   rotate() {
-    if (KEYS_PRESSED['arrowright']) {
-      hero.updateAngle(-1);
+    let delta = degToRad(1);
+    if (MOUSE.isDown) {
+      hero.updateAngle(getSegmentVector(this.position, MOUSE.position));
+    } else if (KEYS_PRESSED['arrowright']) {
+      hero.updateAngle(this.angle - delta);
     } else if (KEYS_PRESSED['arrowleft']) {
-      hero.updateAngle(1);
+      hero.updateAngle(this.angle + delta);
     }
   },
   updateAngle(angle) {
-    this.angle += angle;
-
-    if (this.angle > 360) this.angle = 0;
-    else if (this.angle < 0) this.angle = 360;
-
-    const angleRad = degToRad(this.angle);
-    this.directionVector = [Math.sin(angleRad), Math.cos(angleRad)];
+    if (Array.isArray(angle)) {
+      angle = Math.atan2(1, 0) - Math.atan2(angle[1], angle[0]);
+    }
+    this.angle = angle;
+    this.directionVector = [Math.sin(angle), Math.cos(angle)];
   },
   updatePosition(newPoint) {
     const hasCollided = obstacles.some(
@@ -94,6 +91,7 @@ const hero = {
 const ray = {
   segments: [],
   collidedObject: null,
+  /** return tuple of [obstacle,distanceToIt] */
   getClosestObstacle(point) {
     return obstacles.reduce(
       (acc, circle) => {
@@ -105,9 +103,6 @@ const ray = {
       },
       [null, Number.MAX_SAFE_INTEGER],
     );
-  },
-  getCollisionPoint() {
-    return this.segments[this.segments.length - 1][0];
   },
   getSegments(origin, directionVector) {
     this.segments = [];
@@ -125,8 +120,11 @@ const ray = {
 
     this.collidedObject = distance < 0.1 ? closestObstacle : null;
   },
+  getCollisionPoint() {
+    return this.segments[this.segments.length - 1][0];
+  },
   render() {
-    /** Draw ray march */
+    ctx.lineWidth = 2;
 
     this.segments.forEach(([center, radius], i) => {
       ctx.beginPath();
@@ -142,16 +140,17 @@ const ray = {
       ctx.fill();
       ctx.globalAlpha = 1;
       ctx.beginPath();
-      ctx.fillStyle = 'rgba(255,0,0,0.4)';
-      ctx.arc(...center, 2, 0, Math.PI * 2);
+      ctx.fillStyle = 'rgba(255,255,255,0.4)';
+      ctx.arc(...center, 3, 0, Math.PI * 2);
       ctx.fill();
     });
 
-    /** Draw ray */
     ctx.beginPath();
     ctx.moveTo(...hero.position);
     ctx.lineTo(...this.getCollisionPoint());
     ctx.stroke();
+
+    ctx.lineWidth = 1;
   },
 };
 
@@ -167,27 +166,38 @@ const loop = () => {
   ray.getSegments(hero.position, hero.directionVector);
 
   ctx.fillStyle = 'rgba(255,255,255,0.2)';
-  ctx.strokeStyle = 'rgba(255,255,255,0.4)';
 
   obstacles.forEach(obstacle => {
     ctx.beginPath();
+    // ctx.fillStyle = obstacle.color;
     ctx.arc(...obstacle.center, obstacle.radius, 0, Math.PI * 2);
     ctx.fill();
-    ctx.stroke();
   });
 
   ray.render();
 };
 
 const init = () => {
-  function updateCanvasSize() {
+  const isMovementKey = key =>
+    key === 'w' ||
+    key === 's' ||
+    key === 'd' ||
+    key === 'a' ||
+    key === 'arrowleft' ||
+    key === 'arrowright' ||
+    key === 'arrowup' ||
+    key === 'arrowdown';
+
+  const updateCanvasSize = () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     canvasDiagonalLength = Math.sqrt(canvas.width ** 2 + canvas.height ** 2);
     return updateCanvasSize;
-  }
+  };
+  updateCanvasSize();
 
-  function onKeyup(e) {
+  window.addEventListener('resize', updateCanvasSize);
+  window.addEventListener('keyup', e => {
     delete KEYS_PRESSED[e.key.toLowerCase()];
     if (e.key === ' ' || e.code === 'Space') {
       if (animationRequest) {
@@ -197,21 +207,24 @@ const init = () => {
         animationRequest = requestAnimationFrame(loop);
       }
     }
-  }
-
-  function onKeydown(e) {
+  });
+  window.addEventListener('keydown', e => {
     const key = e.key.toLowerCase();
 
     KEYS_PRESSED[key] = true;
     if (isMovementKey(key)) {
       e.preventDefault();
     }
-  }
-
-  updateCanvasSize();
-  window.addEventListener('resize', updateCanvasSize);
-  window.addEventListener('keyup', onKeyup);
-  window.addEventListener('keydown', onKeydown);
+  });
+  canvas.addEventListener('mousemove', e => {
+    MOUSE.position = [e.clientX, e.clientY];
+  });
+  canvas.addEventListener('mouseup', e => {
+    MOUSE.isDown = false;
+  });
+  canvas.addEventListener('mousedown', e => {
+    MOUSE.isDown = true;
+  });
 
   /** Start position and angle */
   hero.updatePosition([canvas.width / 2, canvas.height / 2]);
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,10 @@
-// export const EPS = 0.0000001;
+export function degToRad(deg) {
+  return deg * (Math.PI / 180);
+}
+
+export function radToDeg(rad) {
+  return rad * (180 / Math.PI);
+}
 
 export function truncateBetween(val, min, max) {
   return Math.max(min, Math.min(val, max));
@@ -12,11 +18,6 @@ export function getNormalizedVector([x, y]) {
   const mod = getVectorModulus([x, y]);
   return [x / mod, y / mod];
 }
-
-export function degToRad(deg) {
-  return deg * (Math.PI / 180);
-}
-
 export function getDistanceBetweenPoints([x1, y1], [x2, y2]) {
   return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
@@ -24,98 +25,14 @@ export function getDistanceBetweenPoints([x1, y1], [x2, y2]) {
 export function signedDistanceToCircle([px, py], { center: [cx, cy], radius }) {
   return getDistanceBetweenPoints([cx, cy], [px, py]) - radius;
 }
+export function getDotProduct([x1, y1], [x2, y2]) {
+  return x1 * x2 + y1 * y2;
+}
 
-// function segmentToGeneralForm([[x1, y1], [x2, y2]]) {
-//   const a = y1 - y2;
-//   const b = x2 - x1;
-//   const c = x2 * y1 - x1 * y2;
-//   return [a, b, c];
-// }
+export function getCrossProduct([x1, y1], [x2, y2]) {
+  return x1 * y2 - x2 * y2;
+}
 
-// // Checks if a point 'pt' is inside the rect defined by (x1,y1), (x2,y2)
-// function isPointInRectangle([px, py], [x1, y1, x2, y2]) {
-//   const x = Math.min(x1, x2);
-//   const y = Math.min(y1, y2);
-//   const X = Math.max(x1, x2);
-//   const Y = Math.max(y1, y2);
-//   return x - EPS <= px && px <= X + EPS && y - EPS <= py && py <= Y + EPS;
-// }
-
-// export function getLineCircleIntersection(circle, [a, b, c]) {
-//   const [cx, cy, r] = circle;
-
-//   // Solve for the variable x with the formulas: ax + by = c (equation of line)
-//   // and (x-X)^2 + (y-Y)^2 = r^2 (equation of circle where X,Y are known) and expand to obtain quadratic:
-//   // (a^2 + b^2)x^2 + (2abY - 2ac + - 2b^2X)x + (b^2X^2 + b^2Y^2 - 2bcY + c^2 - b^2r^2) = 0
-//   // Then use quadratic formula X = (-b +- sqrt(a^2 - 4ac))/2a to find the
-//   // roots of the equation (if they exist) and this will tell us the intersection points
-
-//   // In general a quadratic is written as: Ax^2 + Bx + C = 0
-//   // (a^2 + b^2)x^2 + (2abY - 2ac + - 2b^2X)x + (b^2X^2 + b^2Y^2 - 2bcY + c^2 - b^2r^2) = 0
-//   var A = a ** 2 + b ** 2;
-//   var B = 2 * a * b * cy - 2 * a * c - 2 * b ** 2 * cx;
-//   var C =
-//     b ** 2 * cx ** 2 +
-//     b ** 2 * cy ** 2 -
-//     2 * b * c * cy +
-//     c * c -
-//     b ** 2 * r ** 2;
-
-//   // Use quadratic formula x = (-b +- sqrt(a^2 - 4ac))/2a to find the
-//   // roots of the equation (if they exist).
-
-//   var D = B * B - 4 * A * C;
-//   var x1, y1, x2, y2;
-
-//   // Handle vertical line case with b = 0
-//   if (Math.abs(b) < EPS) {
-//     // Line equation is ax + by = c, but b = 0, so x = c/a
-//     x1 = c / a;
-
-//     // No intersection
-//     if (Math.abs(cx - x1) > r) return [];
-
-//     // Vertical line is tangent to circle
-//     if (Math.abs(x1 - r - cx) < EPS || Math.abs(x1 + r - cx) < EPS)
-//       return [[x1, cy]];
-
-//     var dx = Math.abs(x1 - cx);
-//     var dy = Math.sqrt(r * r - dx * dx);
-
-//     // Vertical line cuts through circle
-//     return [[x1, cy + dy], [x1, cy - dy]];
-
-//     // Line is tangent to circle
-//   } else if (Math.abs(D) < EPS) {
-//     x1 = -B / (2 * A);
-//     y1 = (c - a * x1) / b;
-
-//     return [[x1, y1]];
-
-//     // No intersection
-//   } else if (D < 0) {
-//     return [];
-//   } else {
-//     D = Math.sqrt(D);
-
-//     x1 = (-B + D) / (2 * A);
-//     y1 = (c - a * x1) / b;
-
-//     x2 = (-B - D) / (2 * A);
-//     y2 = (c - a * x2) / b;
-
-//     return [[x1, y1], [x2, y2]];
-//   }
-// }
-
-// export function getSegmentCircleIntersection(segment, circle) {
-//   const lineGeneralForm = segmentToGeneralForm(segment);
-//   const lineIntersections = getLineCircleIntersection(circle, lineGeneralForm);
-
-//   if (lineIntersections.length === 0) return [];
-
-//   const [[x1, y1], [x2, y2]] = segment;
-//   return lineIntersections.filter(point =>
-//     isPointInRectangle(point, [x1, y1, x2, y2]),
-//   );
-// }
+export function getSegmentVector([x1, y1], [x2, y2]) {
+  return [x2 - x1, y2 - y1];
+}
